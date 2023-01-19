f9815331332ddf485222fa9dd5b752f983729ff0
elvis <cenelvis@gmail.com>
2023-01-18 23:19:54 -0500

    28. Function Type + Callbacks

--- app.js
+++ app.js
@@ -5,9 +5,17 @@ function add(n1, n2) {
 function printResult(num) {
     console.log("Result: " + num);
 }
+function addAndHandle(n1, n2, cb) {
+    var result = n1 + n2;
+    cb(result);
+}
 printResult(add(5, 10));
 // let someValue: undefined
 // Function Types
-var combineValues;
+var combineValues; // Specifying how the function should look like
+// let combineValues: Function; // To accept any function
 combineValues = add;
 console.log(combineValues(5, 5));
+addAndHandle(10, 20, function (result) {
+    console.log(result);
+});
--- app.ts
+++ app.ts
@@ -7,6 +7,13 @@ function printResult(num: number): void {
   console.log("Result: " + num);
 }
 
+// Function Type + Callback
+function addAndHandle(n1: number, n2: number, cb: (num: number) => void) {
+  // void = no need to return anything
+  const result = n1 + n2;
+  cb(result);
+}
+
 printResult(add(5, 10));
 
 // let someValue: undefined
@@ -19,3 +26,8 @@ let combineValues: (a: number, b: number) => number; // Specifying how the funct
 combineValues = add;
 
 console.log(combineValues(5, 5));
+
+// Does not need to return anything because of "void"
+addAndHandle(10, 20, (result) => {
+  console.log(result);
+});
